9f06cbb00f99f8d7d93373bf35209376c3eaa315
Gustave Lyman <gustave.lyman@gmail.com>
2018-03-23 17:04:54 +0700

    arief

--- src/com/phiintegration/gitPHITutor/HelloWorld.java
+++ src/com/phiintegration/gitPHITutor/HelloWorld.java
@@ -3,6 +3,9 @@ package com.phiintegration.gitPHITutor;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Haii ");
+		
+		System.out.println("Arief itu hebat");
 	}
 
 }
